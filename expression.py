d6007da51938f87f96b82a2f46058c3fdd63778a
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-03-11 15:33:26 +0530

    Added basic three variable equation solving and more examples

--- expression.py
+++ expression.py
@@ -1,23 +1,36 @@
 import tkinter as tk
 from math import *
+from z3 import *
+from io import StringIO
+import sys
+
 
 def evaluate(event):
-    res.configure(text="Result : " + str(eval(entry.get())))
-
+    try:
+        res.configure(text="Result : " + str(eval(entry.get())))
+    except:
+        x, y, z = Ints('x y z')
+        exp = entry.get()
+        f = eval(exp)
+        result = StringIO()
+        sys.stdout = result
+        solve(f)
+        result_string = result.getvalue()
+        res.configure(text="Result : " + str(result_string))
 
 w = tk.Tk()
 frame = tk.Frame(w)
 w.title("Expression Calculator")
-tk.Label(w, text="Your Expression:").pack(padx = 10, pady = 10)
+tk.Label(w, text="Your Expression:").pack(padx=10, pady=10)
 entry = tk.Entry(w)
 entry.bind("<Return>", evaluate)
-entry.pack(padx = 100, pady = 10)
+entry.pack(padx=100, pady=10)
 res = tk.Label(w)
-res.pack(padx = 10, pady = 10)
+res.pack(padx=10, pady=10)
 button = tk.Button(frame,
                    text="QUIT",
                    fg="red",
                    command=quit)
 frame.pack()
-button.pack(pady = 10)
+button.pack(pady=10)
 w.mainloop()
